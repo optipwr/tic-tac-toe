16071e95730891c07a4257417c8d1f27058ebac0
Michael Irby <michael.irbstoomuch@gmail.com>
2016-12-08 10:54:46 -0500

    start of project

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// 1. Set up Board
+// 2. User should be able to click a box a Mark shows up.
+// --put an onclick in the first square.
+// --when user clicks, call function that puts an X in the box.
+// MILESTONE
+
+function markSquare(square){
+	console.log(square.id);
+}
